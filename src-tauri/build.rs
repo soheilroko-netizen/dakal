8b94b8594552f033b0ce9f40b02e56eb040a99d7
soheilroko-netizen <soheil@dakal.dev>
2026-07-24 02:19:31 +0000

    fix: use .rc wrapper for manifest (RC.EXE needs .rc, not XML) + embed-resource

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,9 +1,5 @@
 fn main() {
     tauri_build::build();
     #[cfg(target_os = "windows")]
-    {
-        let mut res = winres::WindowsResource::new();
-        res.set_manifest_file("manifest.xml");
-        res.compile().unwrap();
-    }
+    embed_resource::compile("manifest.rc", embed_resource::NONE);
 }
