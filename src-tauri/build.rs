a0498a7923598a8bffda47b3cc70b2670fb4a61f
soheilroko-netizen <soheil@dakal.dev>
2026-07-24 02:12:19 +0000

    fix: replace embed-resource with winres (RC.EXE can't compile XML)

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,9 @@
 fn main() {
     tauri_build::build();
     #[cfg(target_os = "windows")]
-    embed_resource::compile("manifest.xml", embed_resource::NONE);
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_manifest_file("manifest.xml");
+        res.compile().unwrap();
+    }
 }
